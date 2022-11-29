093f79bf899734c9b9b5c384b8bddf8af09ab6bb
Furkan Akın <118662756+FurkanAkin16@users.noreply.github.com>
2022-11-29 18:32:21 +0300

    Add files via upload

---
+++ inheritanceDemo/src/inheritanceDemo/InherintaceDemo.java
@@ -0,0 +1,10 @@
+package inheritanceDemo;
+
+public class InherintaceDemo {
+
+	public static void main(String[] args) {
+		KrediUI kredıUI = new KrediUI();
+		kredıUI.KrediHesapla(new OgretmenKrediManager());
+	}
+
+}
