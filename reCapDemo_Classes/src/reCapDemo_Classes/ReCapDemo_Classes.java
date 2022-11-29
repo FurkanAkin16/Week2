8be2b0c4c2b64c333dc54b092b1209d343fcfc59
Furkan Akın <118662756+FurkanAkin16@users.noreply.github.com>
2022-11-29 18:30:43 +0300

    Add files via upload

---
+++ reCapDemo_Classes/src/reCapDemo_Classes/ReCapDemo_Classes.java
@@ -0,0 +1,12 @@
+package reCapDemo_Classes;
+
+public class ReCapDemo_Classes {
+
+	public static void main(String[] args) {
+		DortIslem dortıslem = new DortIslem();
+		int sonuc = dortıslem.Topla(3, 4);
+		System.out.println(sonuc);
+
+	}
+
+}
